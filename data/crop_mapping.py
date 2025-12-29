0c1b0acb27b58e0a3b77c83795961c6ff60751c0
lakshitha <lakshitham022@gmail.com>
2025-12-29 15:25:59 +0530

    Create crop_mapping.py

---
+++ data/crop_mapping.py
@@ -0,0 +1,26 @@
+soil_crop_map = {
+    "Sandy": [
+        "Carrot",
+        "Groundnut",
+        "Watermelon",
+        "Cucumber"
+    ],
+    "Clay": [
+        "Rice",
+        "Broccoli",
+        "Cabbage",
+        "Soybean"
+    ],
+    "Loamy": [
+        "Wheat",
+        "Tomato",
+        "Sugarcane",
+        "Cotton"
+    ],
+    "Silt": [
+        "Potato",
+        "Onion",
+        "Beans",
+        "Lettuce"
+    ]
+}
